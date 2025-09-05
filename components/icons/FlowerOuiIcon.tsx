0d472415c85985761b7e4eb112300f5105bbd82c
bokesi <maxenceposkin@macbookpro.home>
2025-09-06 01:04:42 +0200

    Update configuration for GitHub Pages deployment

--- components/icons/FlowerOuiIcon.tsx
+++ components/icons/FlowerOuiIcon.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface IconProps {
@@ -7,20 +6,13 @@ interface IconProps {
 }
 
 const FlowerOuiIcon: React.FC<IconProps> = ({ size = 24, className = '' }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
+  <img
+    src="components/icons/STR-oui.png"
     width={size}
     height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor"
     className={className}
-    stroke="currentColor"
-    strokeWidth="1"
-    aria-hidden="true"
-  >
-    <path d="M12 22v-3" />
-    <path fillRule="evenodd" d="M12 19a7 7 0 1 0 0-14 7 7 0 0 0 0 14zm0-5a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-  </svg>
+    alt="Blooming flower representing a completed daily goal"
+  />
 );
 
 export default FlowerOuiIcon;
